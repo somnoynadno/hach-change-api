db7daf3da9a40e3c55e790ef5f9b24c2bda53c59
Alexander Zorkin <somnoynadno@yandex.ru>
2021-11-27 21:30:17 +0700

    Preload fix

--- models/entities/User.go
+++ models/entities/User.go
@@ -15,8 +15,8 @@ type User struct {
 	IsVerified  bool        `json:"isVerified" gorm:"not null;default:false;"`
 	LastLogin   *time.Time  `json:"lastLogin"`
 	LastVisit   *time.Time  `json:"lastVisit"`
-	BlogPosts   []*BlogPost `json:"blogPosts,omitempty"`
-	Comments    []*Comment  `json:"comments,omitempty"`
+	BlogPosts   []*BlogPost `json:"blogPosts,omitempty" gorm:"foreignKey:AuthorID"`
+	Comments    []*Comment  `json:"comments,omitempty" gorm:"foreignKey:AuthorID"`
 	Publishers  []*User     `gorm:"many2many:subscriptions" json:"publishers,omitempty"`
 	Subscribers []*User     `gorm:"many2many:subscriptions" json:"subscribers,omitempty"`
 }
