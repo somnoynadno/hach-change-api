624a28a3e6421539ebd513a311e833ad3987e179
Alexander Zorkin <somnoynadno@yandex.ru>
2021-11-27 17:15:26 +0700

    many2many fix 3.0

--- models/entities/User.go
+++ models/entities/User.go
@@ -1,20 +1,21 @@
 package entities
 
 import (
-	"github.com/jinzhu/gorm"
+	"hack-change-api/models/auxiliary"
 	"time"
 )
 
 type User struct {
-	gorm.Model
-	Email      string `gorm:"unique_index;not null;"`
-	Password   string `json:"-" gorm:"not null;"`
-	Name       string
-	Surname    string
-	IsVerified bool `gorm:"not null;default:false;"`
-	LastLogin  *time.Time
-	LastVisit  *time.Time
-	BlogPosts  []*BlogPost
-	Comments   []*Comment
-	Followers  []*User `gorm:"many2many:user_followers"`
+	auxiliary.BaseModel
+	Email         string `gorm:"unique_index;not null;"`
+	Password      string `json:"-" gorm:"not null;"`
+	Name          string
+	Surname       string
+	IsVerified    bool `gorm:"not null;default:false;"`
+	LastLogin     *time.Time
+	LastVisit     *time.Time
+	BlogPosts     []*BlogPost
+	Comments      []*Comment
+	Followers     []*User `gorm:"many2many:followers_subscriptions;ForeignKey:follower_id;References:id"`
+	Subscriptions []*User `gorm:"many2many:followers_subscriptions;ForeignKey:subscription_id;References:id"`
 }
